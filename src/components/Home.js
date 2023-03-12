fb4017a3b3d2bb1f2cab5309009244fb6b64a439
thiwanka778 <thiwankaudumulla@gmail.com>
2023-03-12 16:59:18 +0530

    add link

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,7 +11,7 @@ const Home = () => {
 <p style={{fontSize:"24px",letterSpacing:"2px",padding:"5px"}}>Thiwanka Udumulla</p>
 <p style={{fontSize:"13px",padding:"0px",color:"#d7dae0",}}>Fullstack Developer</p>
 <div className="btns">
-  <a href="https://drive.google.com/u/0/uc?id=1A9JiU-_EFi1TRKL_TlawkftgR8RZX7H1&export=download" target="_blank" className="download">Download CV</a>
+  <a href="https://drive.google.com/u/0/uc?id=1Alx_uBIjNukEd64yWgtbU7myF00TpjQg&export=download" target="_blank" className="download">Download CV</a>
   <HashLink to="#contact" smooth duration={1000} className="talk">Let's Talk</HashLink>
 </div>
    </div>
